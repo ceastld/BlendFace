866ca3390c6a70ddc4a9b25137f8437c1925c960
ceastld <1950946176@qq.com>
2025-01-27 21:49:10 +0800

    env

--- create_env.sh
+++ create_env.sh
@@ -1,4 +1,5 @@
+ml cuDNN/8.4.1.50-CUDA-11.6.0
+
 conda create -n blendface python=3.8 -y
 conda activate blendface
-ml cuDNN/8.4.1.50-CUDA-11.6.0
 pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116
